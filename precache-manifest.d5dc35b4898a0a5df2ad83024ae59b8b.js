a1bc6e875b120d3ae8ddbe9ada15e7d9c4541912
inggan <k6551558@gmail.com>
2019-08-05 13:35:05 +0900

    Updates

---
+++ precache-manifest.d5dc35b4898a0a5df2ad83024ae59b8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6be2cb65af387349140c12f65ad1b623",
+    "url": "/Movie_app/index.html"
+  },
+  {
+    "revision": "0fafc37580b14b6956a7",
+    "url": "/Movie_app/static/css/main.a17fce39.chunk.css"
+  },
+  {
+    "revision": "5cdb4062c6dfdd077298",
+    "url": "/Movie_app/static/js/2.31ebf22a.chunk.js"
+  },
+  {
+    "revision": "0fafc37580b14b6956a7",
+    "url": "/Movie_app/static/js/main.25e8a9ec.chunk.js"
+  },
+  {
+    "revision": "bc3a21fec6a0d0ce35f7",
+    "url": "/Movie_app/static/js/runtime~main.bdf1a524.js"
+  }
+]);
